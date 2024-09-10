bdff210d1c1dd1ab571467566a03fae5fb127c63
chigbatapaskilo <149632981+chigbatapaskilo@users.noreply.github.com>
2024-09-10 13:21:09 +0100

    adding the correct to budget route

--- router/budgetRouter.js
+++ router/budgetRouter.js
@@ -3,10 +3,10 @@ const budgetRoute=express.Router()
 const { authorize}=require('../middleware/auth');
 const { createBudget, saveForTarget, budgetHistory, budgetHistoryForAcategory, getOneBudget, deleteBudget,  } = require('../controller/budgetController');
 
-debtRoute.post('/create/:categoryId',authorize,createBudget)
-debtRoute.put('/:budgetId/save/:categoryId',authorize,saveForTarget)
-debtRoute.get('/history',authorize,budgetHistory)
-debtRoute.get('/onebudget/:budgetId',authorize,getOneBudget)
-debtRoute.get('/budgetforcategory/:categoryId',authorize,budgetHistoryForAcategory)
-debtRoute.delete('/:budgetId/delete/:categoryId',authorize,deleteBudget)
+budgetRoute.post('/create/:categoryId',authorize,createBudget)
+budgetRoute.put('/:budgetId/save/:categoryId',authorize,saveForTarget)
+budgetRoute.get('/history',authorize,budgetHistory)
+budgetRoute.get('/onebudget/:budgetId',authorize,getOneBudget)
+budgetRoute.get('/budgetforcategory/:categoryId',authorize,budgetHistoryForAcategory)
+budgetRoute.delete('/:budgetId/delete/:categoryId',authorize,deleteBudget)
 module.exports=budgetRoute
